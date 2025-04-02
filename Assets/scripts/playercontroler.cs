84f3be2c82cf1c7cf536fdf498bcfc423d43826e
andres-luna501 <andres.luna23B@udgvirtual.udg.mx>
2025-04-01 19:10:04 -0600

    Primer commit

---
+++ Assets/scripts/playercontroler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        // Se ejecuta una vez al iniciar el juego
+        Debug.Log("hola mundo");
+    }
+
+    void Update()
+    {
+        // Se ejecuta en cada frame
+    }
+}
